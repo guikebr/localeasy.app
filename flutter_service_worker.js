7423f5e34bcd9745b07423f3260ad99b5b7a986a
guilherme santos <guilhermesantos130997@gmail.com>
2023-01-12 11:42:39 -0300

    atualizado rota url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -348,8 +348,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "383fdd08084f1782cd702a624a8c7289",
-"/": "383fdd08084f1782cd702a624a8c7289",
+"index.html": "6b5c40c45aaa9f6456890d12311bfa6e",
+"/": "6b5c40c45aaa9f6456890d12311bfa6e",
 "main.dart.js": "c0eafe197ad74f2242d057ecd89aac01",
 "manifest.json": "9a1913510bae53f23fb4cc757491475a",
 "version.json": "5f68212edf4650b368faeb6edf90df81"
